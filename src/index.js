5c669507fb5a601642a30e9f877d4071508ca51f
Perez Hernandez, M <m.perez.hernandez@accenture.com>
2020-03-26 09:29:06 -0600

    click counter, complete demo for module 2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let model = {
+  clicks: 0
+}
+
+function render() {
+  ReactDOM.render(
+    <App 
+      clicks={model.clicks}
+      onClick={ () => { 
+        model.clicks += 1;
+        render();
+      }}/>,
+    document.getElementById('root')
+  );
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
